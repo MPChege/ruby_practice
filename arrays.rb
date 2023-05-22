db2db601002a63d635b3066b82c660cda1238a29
MarkChege <mark.wanyoike@strathmore.edu>
2023-05-23 02:09:38 +0300

    added some arrays

--- arrays.rb
+++ arrays.rb
@@ -1,15 +1,27 @@
 # Arrays
 # Create an array
 friends =Array["Mark Paul", "Karen","Oscar"]
-
+puts friends.sort()
+#assigning a new array 
+# friends[0] = "Hailey"
+# friends[1] = "Lisa"
 # Print the array
-print(friends)
+# puts friends[-1]
+# Print the array
+# puts friends[0]
+# Print the array
+# puts friends[1]
+# Print the array
+# puts friends[2]
+# Print the array
+# puts friends[0,2]
 
-# Print the length of the array
-print(len(friends))
 
-# Sort the array
-friends.sort()
 
-# Print the sorted array
-print(friends)
+
+#Assigning new arrays without giving out initials
+# friends =Array.new
+# friends[0] ="Michael"
+# friends[1] ="Hudson"
+# puts friends [0]
+# puts friends [1]
