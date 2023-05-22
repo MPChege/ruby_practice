cf0a206ada1278420303f270295a0d0679749fae
MarkChege <mark.wanyoike@strathmore.edu>
2023-05-23 01:51:41 +0300

    changed condition ro array

---
+++ arrays.rb
@@ -0,0 +1,15 @@
+# Arrays
+# Create an array
+friends =Array["Mark Paul", "Karen","Oscar"]
+
+# Print the array
+print(friends)
+
+# Print the length of the array
+print(len(friends))
+
+# Sort the array
+friends.sort()
+
+# Print the sorted array
+print(friends)
