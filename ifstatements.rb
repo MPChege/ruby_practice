701714613eb170455f08b5ac637201b2076ac7d1
MarkChege <mark.wanyoike@strathmore.edu>
2023-05-24 00:39:08 +0300

    adding if condition statement

--- ifstatements.rb
+++ ifstatements.rb
@@ -13,4 +13,19 @@ end
 
 # we can use and & or
 
+# IF CONDITIONS
+#CREATE A METHOD FIRST AND THEN RETURN THE LARGEST NUMBER 
+
+def max(num1, num2, num3)
+    if num1 >= num2 and num1 >=num3
+        return num1
+    elsif num2 >= num1 and num2 >=num3
+        return num2
+    else
+        return num3
+end
+end
+
+puts max(90,20,3)
+
 
